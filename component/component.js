841fbf2cd869cc8ae670e8f3f4180a62f2c8c37b
daiyuzeng <cridequeger@yahoo.com>
2020-05-26 11:24:12 +0800

    fix(ui-cluster-driver-aliyun): regions is loaded from aliyun api now

--- component/component.js
+++ component/component.js
@@ -102,6 +102,13 @@ const REGIONS = [
     label: 'cn-zhangjiakou',
     value: 'cn-zhangjiakou'
   }, {
+    label: 'cn-huhehaote',
+    value: 'cn-huhehaote'
+  }, {
+    label:   'cn-hangzhou',
+    value:   'cn-hangzhou',
+    managed: true,
+  }, {
     label:   'cn-shanghai',
     value:   'cn-shanghai',
     managed: true,
@@ -109,15 +116,11 @@ const REGIONS = [
     label: 'cn-shenzhen',
     value: 'cn-shenzhen'
   }, {
-    label:   'cn-hangzhou',
-    value:   'cn-hangzhou',
-    managed: true,
+    label: 'cn-chengdu',
+    value: 'cn-chengdu'
   }, {
     label: 'cn-hongkong',
     value: 'cn-hongkong'
-  }, {
-    label: 'cn-huhehaote',
-    value: 'cn-huhehaote'
   }, {
     label: 'ap-northeast-1',
     value: 'ap-northeast-1'
@@ -133,6 +136,10 @@ const REGIONS = [
     label: 'ap-southeast-2',
     value: 'ap-southeast-2'
   }, {
+    label:   'ap-southeast-3',
+    value:   'ap-southeast-3',
+    managed: true,
+  }, {
     label:   'ap-southeast-5',
     value:   'ap-southeast-5',
     managed: true,
@@ -147,12 +154,11 @@ const REGIONS = [
     value:   'me-east-1',
     managed: false
   }, {
+    label: 'eu-west-1',
+    value: 'eu-west-1'
+  }, {
     label: 'eu-central-1',
     value: 'eu-central-1'
-  }, {
-    label:   'ap-southeast-3',
-    value:   'ap-southeast-3',
-    managed: true,
   }];
 
 const NODECIDRMASKS = [
@@ -238,8 +244,8 @@ const PLATFORMTYPES = [
 ];
 
 const languages = {
-  'en-us':   {"clusterNew":{"aliyunkcs":{"label":"Aliyun Kubernetes Container Service","shortLabel":"Alibaba ACK","access":{"next":"Next: Configure Cluster","loading":"Loading Zones from Alibaba ACK","title":"Account Access","detail":"Choose the region and API Key that will be used to launch Alibaba Kubernetes Service"},"cluster":{"title":"Cluster Configuration","detail":"Choose the Zone and Kubernetes version that will be used to launch Alibaba Kubernetes Service","next":"Next: Configure Master Nodes","loading":"Loading Availability Zones from Alibaba","name":{"required":"Cluster name is required"}},"clusters":{"k8s":"Dedicated Kubernetes","managed":"Managed Kubernetes","label":"Cluster Type"},"master":{"title":"Master Nodes","detail":"Configure the master nodes that will be used to launch Alibaba Kubernetes Service","next":"Next: Configure Worker Nodes","loading":"Loading Key Pairs from Alibaba"},"worker":{"title":"Worker Nodes","detail":"Configure the worker nodes that will be used to launch Alibaba Kubernetes Service"},"disk":{"cloud":"Ordinary Disk","ephemeralSsd":"Local SSD Disk","efficiency":"Ultra Disk","ssd":"SSD Disk","essd":"ESSD Disk"},"rootSize":{"label":"Root Disk Size","placeholder":"e.g. 120"},"rootType":{"label":"Root Disk Type"},"storageType":{"label":"Data Disk Type"},"storageSize":{"label":"Data Disk Size","placeholder":"e.g. 120"},"masterNum":{"label":"Master Nodes Count","help":"The count of master nodes will be launched in this Kubernetes cluster"},"numOfNodes":{"label":"Worker Nodes Count","placeholder":"e.g. 3","required":"Worker Nodes Count is required","help":"The count of worker nodes will be launched in this Kubernetes cluster"},"keyPair":{"label":"Key Pair","required":"Key Pair is required"},"region":{"label":"Region"},"vpcId":{"label":"VPC","prompt":"Choose VPC...","required":"VPC is required"},"vswitchId":{"label":"VSwitch","prompt":"Choose VSwitch...","required":"VSwitch is required"},"proxyMode":{"label":"Kube-Proxy Mode"},"containerCidr":{"label":"Pod CIDR Block","required":"Pod CIDR Block is required","invalid":"Pod CIDR Block is invalid","placeholder":"Specify a valid CIDR block that contains only internal IP addresses, namely one of the following CIDR blocks or their subnets: 10.0.0.0/8 172.16-31.0.0/12-16 and 192.168.0.0/16.Cannot be duplicated with the VPC and the network segment used by the existing kubernetes cluster in the VPC"},"serviceCidr":{"label":"Service CIDR","required":"Service CIDR is required","invalid":"Service CIDR is invalid","placeholder":"Valid CIDR blocks include: 10.0.0.0/8 172.16-31.0.0/12-16 and 192.168.0.0/16.Cannot be duplicated with the VPC and the network segment used by the existing kubernetes cluster in the VPC"},"nodeCidrMask":{"label":"IP Addresses per Node"},"snatEntry":{"label":"Configure SNAT","placeholder":"Configure SNAT for VPC"},"endpointPublicAccess":{"label":"Public Access","placeholder":"Expose API Server with EIP"},"masterInstanceChargeType":{"label":"Billing Method","prePaid":"Subscription","postPaid":"Pay-As-You-Go"},"masterPeriod":{"label":"Duration","placeholder":"Unit(Month)"},"masterAutoRenew":{"label":"Auto Renewal","placeholder":"Enable"},"masterAutoRenewPeriod":{"label":"Auto Renewal Duration","placeholder":"Unit(Month)"},"osType":{"label":"Operating System"},"accessKeyId":{"label":"Access Key","placeholder":"Your Aliyun access key","required":"Access Key is required"},"accessKeySecret":{"label":"Secret Key","placeholder":"Your Aliyun secret key","provided":"Provided","required":"Secret Key is required"},"version":{"label":"Kubernetes Version"},"zoneId":{"label":"Availability Zone","required":"Availability Zone is required"},"instanceType":{"label":"Instance Type","required":"Instance Type is required"}}}},
-  'zh-hans': {"clusterNew":{"aliyunkcs":{"label":"阿里云Kubernetes容器服务","shortLabel":"Alibaba ACK","access":{"next":"下一步: 配置集群","loading":"从Alibaba ACK加载可用区域","title":"帐户访问","detail":"选择将用于启动阿里巴巴Kubernetes服务的地区和API密钥"},"cluster":{"title":"集群配置","detail":"选择将用于启动阿里巴巴Kubernetes服务的区域和Kubernetes版本","next":"下一步: 配置Master节点","loading":"从Alibaba加载可用区域","name":{"required":"集群名称必须输入"}},"clusters":{"k8s":"专有Kubernetes","managed":"托管Kubernetes","label":"集群类型"},"master":{"title":"Master 节点","detail":"配置用于启动阿里巴巴Kubernetes服务的主节点","next":"下一步: 配置Worker节点","loading":"从阿里巴巴加载密钥对"},"worker":{"title":"Worker 节点","detail":"配置用于启动阿里巴巴Kubernetes服务的工作节点"},"disk":{"cloud":"普通云盘","ephemeralSsd":"本地SSD盘","efficiency":"高效云盘","ssd":"SSD 云盘","essd":"ESSD 云盘"},"rootSize":{"label":"根磁盘大小","placeholder":"例如: 120"},"rootType":{"label":"根磁盘类型"},"storageType":{"label":"数据磁盘类型"},"storageSize":{"label":"数据磁盘大小","placeholder":"例如: 120"},"masterNum":{"label":"Master 节点数","help":"此Kubernetes集群将运行的Master节点数"},"numOfNodes":{"label":"Worker 节点数","placeholder":"例如: 3","required":"必须设置Worker节点","help":"此Kubernetes集群将运行的Worker节点数"},"keyPair":{"label":"密钥对","required":"必须选择密钥对"},"region":{"label":"区域"},"vpcId":{"label":"专有网络","prompt":"选择专有网络...","required":"必须选择专有网络"},"vswitchId":{"label":"交换机","prompt":"选择交换机...","required":"必须选择交换机"},"proxyMode":{"label":"kube-proxy 代理模式"},"containerCidr":{"label":"Pod 网络 CIDR","required":"必须输入Pod 网络 CIDR","invalid":"Pod 网络 CIDR 网段格式不合法","placeholder":"请填写有效的私有网段，即以下网段及其子网：10.0.0.0/8，172.16-31.0.0/12-16，192.168.0.0/16。不能与VPC及VPC内已有kubernetes集群使用的网段重复"},"serviceCidr":{"label":"Service CIDR","required":"必须输入Service CIDR","invalid":"Service CIDR 网段不可选","placeholder":"可选范围：10.0.0.0/16-24，172.16-31.0.0/16-24，192.168.0.0/16-24。不能与VPC及VPC内已有kubernetes集群使用的网段重复"},"nodeCidrMask":{"label":"节点 IP 数量"},"snatEntry":{"label":"配置 SNAT","placeholder":"为专有网络配置 SNAT"},"endpointPublicAccess":{"label":"公网访问","placeholder":"使用 EIP 暴露 API Server"},"masterInstanceChargeType":{"label":"付费类型","prePaid":"包年包月","postPaid":"按量付费"},"masterPeriod":{"label":"购买时长","placeholder":"单位(月)"},"masterAutoRenew":{"label":"自动续费","placeholder":"开启"},"masterAutoRenewPeriod":{"label":"自动续费时长","placeholder":"单位(月)"},"osType":{"label":"操作系统"},"accessKeyId":{"label":"访问秘钥","placeholder":"请输入你的访问秘钥","required":"访问秘钥必须输入"},"accessKeySecret":{"label":"访问秘钥令牌","placeholder":"请输入你的访问秘钥令牌","provided":"Provided","required":"访问秘钥令牌必须输入"},"version":{"label":"Kubernetes版本"},"zoneId":{"label":"可用区域","required":"可用区域必须设置"},"instanceType":{"label":"实例类型","required":"实例类型必须设置"}}}}
+  'en-us':   {"clusterNew":{"aliyunkcs":{"label":"Aliyun Kubernetes Container Service","shortLabel":"Alibaba ACK","access":{"next":"Next: Configure Cluster","loading":"Loading Zones from Alibaba ACK","title":"Account Access","detail":"Choose the region and API Key that will be used to launch Alibaba Kubernetes Service"},"cluster":{"title":"Cluster Configuration","detail":"Choose the Zone and Kubernetes version that will be used to launch Alibaba Kubernetes Service","next":"Next: Configure Master Nodes","loading":"Loading Availability Zones from Alibaba","name":{"required":"Cluster name is required"}},"clusters":{"k8s":"Dedicated Kubernetes","managed":"Managed Kubernetes","label":"Cluster Type"},"master":{"title":"Master Nodes","detail":"Configure the master nodes that will be used to launch Alibaba Kubernetes Service","next":"Next: Configure Worker Nodes","loading":"Loading Key Pairs from Alibaba"},"worker":{"title":"Worker Nodes","detail":"Configure the worker nodes that will be used to launch Alibaba Kubernetes Service"},"disk":{"cloud":"Ordinary Disk","ephemeralSsd":"Local SSD Disk","efficiency":"Ultra Disk","ssd":"SSD Disk","essd":"ESSD Disk"},"rootSize":{"label":"Root Disk Size","placeholder":"e.g. 120"},"rootType":{"label":"Root Disk Type"},"storageType":{"label":"Data Disk Type"},"storageSize":{"label":"Data Disk Size","placeholder":"e.g. 120"},"masterNum":{"label":"Master Nodes Count","help":"The count of master nodes will be launched in this Kubernetes cluster"},"numOfNodes":{"label":"Worker Nodes Count","placeholder":"e.g. 3","required":"Worker Nodes Count is required","help":"The count of worker nodes will be launched in this Kubernetes cluster"},"keyPair":{"label":"Key Pair","required":"Key Pair is required"},"region":{"label":"Region"},"vpcId":{"label":"VPC","prompt":"Choose VPC...","required":"VPC is required"},"vswitchId":{"label":"VSwitch","prompt":"Choose VSwitch...","required":"VSwitch is required"},"proxyMode":{"label":"Kube-Proxy Mode"},"containerCidr":{"label":"Pod CIDR Block","required":"Pod CIDR Block is required","invalid":"Pod CIDR Block is invalid","placeholder":"Specify a valid CIDR block that contains only internal IP addresses, namely one of the following CIDR blocks or their subnets: 10.0.0.0/8 172.16-31.0.0/12-16 and 192.168.0.0/16.Cannot be duplicated with the VPC and the network segment used by the existing kubernetes cluster in the VPC"},"serviceCidr":{"label":"Service CIDR","required":"Service CIDR is required","invalid":"Service CIDR is invalid","placeholder":"Valid CIDR blocks include: 10.0.0.0/8 172.16-31.0.0/12-16 and 192.168.0.0/16.Cannot be duplicated with the VPC and the network segment used by the existing kubernetes cluster in the VPC"},"nodeCidrMask":{"label":"IP Addresses per Node"},"snatEntry":{"label":"Configure SNAT","placeholder":"Configure SNAT for VPC"},"endpointPublicAccess":{"label":"Public Access","placeholder":"Expose API Server with EIP"},"masterInstanceChargeType":{"label":"Billing Method","prePaid":"Subscription","postPaid":"Pay-As-You-Go"},"masterPeriod":{"label":"Duration","placeholder":"Unit(Month)"},"masterAutoRenew":{"label":"Auto Renewal","placeholder":"Enable"},"masterAutoRenewPeriod":{"label":"Auto Renewal Duration","placeholder":"Unit(Month)"},"platform":{"label":"Operating System"},"accessKeyId":{"label":"Access Key","placeholder":"Your Aliyun access key","required":"Access Key is required"},"accessKeySecret":{"label":"Secret Key","placeholder":"Your Aliyun secret key","provided":"Provided","required":"Secret Key is required"},"version":{"label":"Kubernetes Version"},"zoneId":{"label":"Availability Zone","required":"Availability Zone is required"},"instanceType":{"label":"Instance Type","required":"Instance Type is required"}}}},
+  'zh-hans': {"clusterNew":{"aliyunkcs":{"label":"阿里云Kubernetes容器服务","shortLabel":"Alibaba ACK","access":{"next":"下一步: 配置集群","loading":"从Alibaba ACK加载可用区域","title":"帐户访问","detail":"选择将用于启动阿里巴巴Kubernetes服务的地区和API密钥"},"cluster":{"title":"集群配置","detail":"选择将用于启动阿里巴巴Kubernetes服务的区域和Kubernetes版本","next":"下一步: 配置Master节点","loading":"从Alibaba加载可用区域","name":{"required":"集群名称必须输入"}},"clusters":{"k8s":"专有Kubernetes","managed":"托管Kubernetes","label":"集群类型"},"master":{"title":"Master 节点","detail":"配置用于启动阿里巴巴Kubernetes服务的主节点","next":"下一步: 配置Worker节点","loading":"从阿里巴巴加载密钥对"},"worker":{"title":"Worker 节点","detail":"配置用于启动阿里巴巴Kubernetes服务的工作节点"},"disk":{"cloud":"普通云盘","ephemeralSsd":"本地SSD盘","efficiency":"高效云盘","ssd":"SSD 云盘","essd":"ESSD 云盘"},"rootSize":{"label":"根磁盘大小","placeholder":"例如: 120"},"rootType":{"label":"根磁盘类型"},"storageType":{"label":"数据磁盘类型"},"storageSize":{"label":"数据磁盘大小","placeholder":"例如: 120"},"masterNum":{"label":"Master 节点数","help":"此Kubernetes集群将运行的Master节点数"},"numOfNodes":{"label":"Worker 节点数","placeholder":"例如: 3","required":"必须设置Worker节点","help":"此Kubernetes集群将运行的Worker节点数"},"keyPair":{"label":"密钥对","required":"必须选择密钥对"},"region":{"label":"区域"},"vpcId":{"label":"专有网络","prompt":"选择专有网络...","required":"必须选择专有网络"},"vswitchId":{"label":"交换机","prompt":"选择交换机...","required":"必须选择交换机"},"proxyMode":{"label":"kube-proxy 代理模式"},"containerCidr":{"label":"Pod 网络 CIDR","required":"必须输入Pod 网络 CIDR","invalid":"Pod 网络 CIDR 网段格式不合法","placeholder":"请填写有效的私有网段，即以下网段及其子网：10.0.0.0/8，172.16-31.0.0/12-16，192.168.0.0/16。不能与VPC及VPC内已有kubernetes集群使用的网段重复"},"serviceCidr":{"label":"Service CIDR","required":"必须输入Service CIDR","invalid":"Service CIDR 网段不可选","placeholder":"可选范围：10.0.0.0/16-24，172.16-31.0.0/16-24，192.168.0.0/16-24。不能与VPC及VPC内已有kubernetes集群使用的网段重复"},"nodeCidrMask":{"label":"节点 IP 数量"},"snatEntry":{"label":"配置 SNAT","placeholder":"为专有网络配置 SNAT"},"endpointPublicAccess":{"label":"公网访问","placeholder":"使用 EIP 暴露 API Server"},"masterInstanceChargeType":{"label":"付费类型","prePaid":"包年包月","postPaid":"按量付费"},"masterPeriod":{"label":"购买时长","placeholder":"单位(月)"},"masterAutoRenew":{"label":"自动续费","placeholder":"开启"},"masterAutoRenewPeriod":{"label":"自动续费时长","placeholder":"单位(月)"},"platform":{"label":"操作系统"},"accessKeyId":{"label":"访问秘钥","placeholder":"请输入你的访问秘钥","required":"访问秘钥必须输入"},"accessKeySecret":{"label":"访问秘钥令牌","placeholder":"请输入你的访问秘钥令牌","provided":"Provided","required":"访问秘钥令牌必须输入"},"version":{"label":"Kubernetes版本"},"zoneId":{"label":"可用区域","required":"可用区域必须设置"},"instanceType":{"label":"实例类型","required":"实例类型必须设置"}}}}
 };
 
 
@@ -256,7 +262,6 @@ export default Ember.Component.extend(ClusterDriver, {
   configField:       'aliyunEngineConfig',
 
   step:                  1,
-  regionChoices:         REGIONS,
   versionChoices:        VERSIONS,
   managedVersionChoices: VERSIONS,
   nodeCidrMaskChoices:   NODECIDRMASKS,
@@ -267,9 +272,10 @@ export default Ember.Component.extend(ClusterDriver, {
 
   systemDiskChoices:     [],
   dataDiskChoices:       [],
-  storageDiskChoices:    null,
+  regionChoices:         [],
   zoneChoices:           [],
   proxyModeChoices:      MODES,
+  storageDiskChoices:    null,
   sgChoices:             null,
   keyChoices:            null,
   allSubnets:            null,
@@ -392,7 +398,22 @@ export default Ember.Component.extend(ClusterDriver, {
         return;
       }
 
-      this.fetch('Region', 'Regions').then(() => {
+      this.fetch('Region', 'Regions').then((regions) => {
+        const transformRegions = [];
+        REGIONS.forEach((regionTemp) => {
+          const found = regions.findBy('value', get(regionTemp, 'value'));
+
+          if (found) {
+            transformRegions.pushObject({
+              ...regionTemp,
+              label: found.raw.LocalName
+            });
+          } else {
+            transformRegions.pushObject(regionTemp);
+          }
+        });
+        set(this, 'regionChoices', transformRegions);
+
         this.regionDidChange();
         set(this, 'step', 2);
         cb(true);
@@ -697,13 +718,17 @@ export default Ember.Component.extend(ClusterDriver, {
   nodeCidrMaskShowValue: computed('intl.locale', 'config.nodeCidrMask', function() {
     const mask = parseInt(get(this, 'config.nodeCidrMask'));
 
+    if (isNaN(mask)) {
+      return 0;
+    }
+
     return Math.pow(2, (32 - mask));
   }),
 
   vpcShowValue: computed('intl.locale', 'config.vpcId', function() {
     const vpcs = get(this, 'vpcs');
 
-    if (vpcs) {
+    if (vpcs && get(this, 'config.vpcId')) {
       return get(vpcs.findBy('value', get(this, 'config.vpcId')), 'label');
     } else {
       return '';
@@ -713,7 +738,7 @@ export default Ember.Component.extend(ClusterDriver, {
   vswitchShowValue: computed('intl.locale', 'config.vswitchId', function() {
     const vswitches = get(this, 'vswitches');
 
-    if (vswitches) {
+    if (vswitches && get(this, 'config.vswitchId')) {
       return get(vswitches.findBy('value', get(this, 'config.vswitchId')), 'label');
     } else {
       return '';
